e679dd13447146e25fd85283342864d62f1d0e44
Aaron Enyetu <aaronenyetu@gmail.com>
2022-08-01 16:58:23 -0700

    Created test to ass a new owner

---
+++ __tests__/owners.test.js
@@ -0,0 +1,20 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('should add a new owner', async () => {
+    const resp = await (await request(app).post('/owners')).setEncoding({ name: 'Franny' });
+    expect(resp.status).toBe(200);
+
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
