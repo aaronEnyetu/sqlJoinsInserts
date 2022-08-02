5f33299fda12202272eb6d14070abda1733a4f09
Aaron Enyetu <aaronenyetu@gmail.com>
2022-08-02 10:45:54 -0700

    Tests passing

--- __tests__/owners.test.js
+++ __tests__/owners.test.js
@@ -8,10 +8,38 @@ describe('backend-express-template routes', () => {
     return setup(pool);
   });
 
-  it('should add a new owner', async () => {
+  it('#GET /:id should return the owner with its pets', async () => {
+    const resp = await request(app).get('/owners/1');
+    console.log(resp.body);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      pets: expect.any(Array),
+    });
+  });
+
+  it('#POST /owners should add a new owner', async () => {
     const resp = await request(app).post('/owners').send({ name: 'Franny' });
     expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      pets: expect.any(Array),
+    });
 
+  });
+
+  it('#POST / owners should add a new owner and associate it with pets', async () => {
+    const resp = await request(app)
+      .post('/owners')
+      .send({ name: ' Harriet', petIds: [1, 2] });
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      pets: expect.any(Array),
+
+    });
   });
 
   afterAll(() => {
